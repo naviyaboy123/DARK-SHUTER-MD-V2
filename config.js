3e68801de20f0fa815ec6ccb01e5a9d71a4427a7
naviyaboy123 <naviyab979@gmail.com>
2024-12-20 12:21:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: 'put session',
-GITHUB_AUTH_TOKEN: 'put token', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
-GITHUB_USER_NAME: 'put your github user name',
+SESSION_ID: 'DARK-SHUTER-MD=OEcQFDTL#0kWMlz-shVgUMHl4Ke0id2MKM3SUxq89Xz--cU3qoBo',
+GITHUB_AUTH_TOKEN: 'WM90z2DzCLfuocmOlBjROVHTTpYEgQ2d9JGU', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
+GITHUB_USER_NAME: 'DarkeHaker',
 
 };
